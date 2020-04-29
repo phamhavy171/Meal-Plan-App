d7b325e8d1719f69849fa0d985ae89de560c8ebd
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-04-29 08:54:41 +0300

    build API call for recipes with search query

---
+++ frontend/src/Recipe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{calories}</p>
+      <img src={image} alt={title}></img>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Recipe;
