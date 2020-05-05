f52c0b793d0199d0b60af034459b3d7ce22858af
oona-k <59806531+oona-k@users.noreply.github.com>
2020-05-05 15:33:58 +0300

    Created contact form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ContactForm from "./ContactForm/ContactForm";
 
 function App() {
   return (
     <div>
-      
+      <ContactForm />
     </div>
   );
 }
