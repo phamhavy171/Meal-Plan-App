d24eb679d21a47a40555c5004a60fa49b1f7ef46
maaliskesakuu <tanja@prodi.fi>
2020-05-07 15:11:14 +0300

    added container to Login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,52 +1,55 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
+	render() {
+		return (
+			<Container fluid="sm">
+				<form>
+					<h3>Sign In</h3>
 
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
+					<div className="form-group">
+						<label>Email address</label>
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+						/>
+					</div>
 
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
+					<div className="form-group">
+						<label>Password</label>
+						<input
+							type="password"
+							className="form-control"
+							placeholder="Enter password"
+						/>
+					</div>
 
-        <div className="form-group">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
+					<div className="form-group">
+						<div className="custom-control custom-checkbox">
+							<input
+								type="checkbox"
+								className="custom-control-input"
+								id="customCheck1"
+							/>
+							<label className="custom-control-label" htmlFor="customCheck1">
+								Remember me
+							</label>
+						</div>
+					</div>
 
-        <Button variant="info" block>
-          Sign In
-        </Button>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
-    );
-  }
+					<Button variant="info" block>
+						Sign In
+					</Button>
+					<p className="forgot-password text-right">
+						Forgot <a href="#">password?</a>
+					</p>
+				</form>
+			</Container>
+		);
+	}
 }
 
 export default Login;
