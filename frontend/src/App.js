c61de312721d7c221722048991c3c5f3a74f93d6
maaliskesakuu <tanja@prodi.fi>
2020-05-07 13:59:20 +0300

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import ContactForm from "./ContactForm/ContactForm";
+import ContactForm from "./components/ContactForm";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
       <ContactForm />
+      <Signup />
+      <Login />
     </div>
   );
 }
