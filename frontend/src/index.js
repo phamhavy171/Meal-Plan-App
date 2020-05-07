c292ce9720335637c7b66b826736cda9566b36a8
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-05-07 13:44:13 +0300

    combined forms

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App1 from "./App1";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App1 />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
