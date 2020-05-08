d5c4684267a38c5b18aa23544de8a56bbd15b034
maaliskesakuu <tanja@prodi.fi>
2020-05-08 17:26:21 +0300

    added health/allergy and diet filters to SearchPage component plus changed it into class component

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -1,118 +1,116 @@
 import React, { Component } from 'react';
+import Recipe from './Recipe';
+
+const APP_ID = 'dcd2ff3e';
+const APP_KEY = '34c2380ac6cb8b04f580a3e848986ecd';
 
 class SearchPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			recipes: null,
+			query: '',
+			diet: '',
+			health: '',
+		};
+
+		this.getRecipes = this.getRecipes.bind(this);
+		this.getUrl = this.getUrl.bind(this);
+		this.getQuery = this.getQuery.bind(this);
+		this.getDiet = this.getDiet.bind(this);
+		this.getHealth = this.getHealth.bind(this);
+	}
+
+	getUrl() {
+		return `https://api.edamam.com/search?q=${this.state.query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&health=${this.state.health}&diet=${this.state.diet}`;
+	}
+
+	getRecipes(e) {
+		e.preventDefault();
+		const scope = this;
+		fetch(this.getUrl())
+			.then((resp) => resp.json())
+			.then((data) => {
+				scope.setState({
+					recipes: data.hits,
+				});
+				console.log(data.hits);
+			})
+			.catch((err) => {
+				console.log(
+					`An error has occured while fetching data from Edamam ${err}`
+				);
+			});
+	}
+
+	getDiet(e) {
+		this.setState({
+			// diet: e.target.value.toLowerCase(),
+			diet: e.target.value,
+		});
+	}
+
+	getQuery(e) {
+		this.setState({
+			query: e.target.value,
+		});
+	}
+
+	getHealth(e) {
+		this.setState({
+			health: e.target.value,
+		});
+	}
+
 	render() {
 		return (
-			<div>
-				<form>
-					<input type="text"></input>
-					<select>
+			<div className="App">
+				<form onSubmit={this.getRecipes} className="search-form">
+					<input
+						className="search-bar"
+						type="text"
+						value={this.state.query}
+						onChange={this.getQuery}
+						placeholder="Search for something"
+						required
+					/>
+					<select value={this.state.diet} onChange={this.getDiet} required>
 						<option value="none"></option>
-						<option value="Balanced">Balanced</option>
-						<option value="High-Fiber">High-Fiber</option>
-						<option value="High-Protein">High-Protein</option>
-						<option value="Low-Carb">Low-Carb</option>
-						<option value="Low-Fat">Low-Fat</option>
-						<option value="Low-Sodium">Low-Sodium</option>
+						<option value="balanced">Balanced</option>
+						<option value="high-protein">High-Protein</option>
+						<option value="low-carb">Low-Carb</option>
+						<option value="low-fat">Low-Fat</option>
 					</select>
-					<button type="submit">submit</button>
+					<select value={this.state.health} onChange={this.getHealth} required>
+						<option value="none"></option>
+						<option value="vegan">Vegan</option>
+						<option value="vegetarian">Vegetarian</option>
+						<option value="peanut-free">Peanut-Free</option>
+						<option value="true-nut-free">Tree-Nut-Free</option>
+						<option value="alcohol-free">Alcohol-Free</option>
+						<option value="sugar-conscious">Sugar-Conscious</option>
+					</select>
+					<button className="search-button" type="submit">
+						Search
+					</button>
 				</form>
-				{/* <div>
-					{recipes.map((recipe) => (
-						<Recipe
-							key={recipe.recipe.label}
-							title={recipe.recipe.label}
-							calories={recipe.recipe.calories}
-							image={recipe.recipe.image}
-							ingredients={recipe.recipe.ingredients}
-							healthLabels={recipe.recipe.healthLabels}
-						/>
-					))}
-				</div> */}
+				<div className="recipes">
+					{this.state.recipes
+						? this.state.recipes.map((recipe) => (
+								<Recipe
+									key={recipe.recipe.label}
+									title={recipe.recipe.label}
+									calories={recipe.recipe.calories}
+									image={recipe.recipe.image}
+									ingredients={recipe.recipe.ingredients}
+									healthLabels={recipe.recipe.healthLabels}
+								/>
+						  ))
+						: ''}
+				</div>
 			</div>
 		);
 	}
 }
 
 export default SearchPage;
-
-// import React, { useEffect, useState } from 'react';
-// import Recipe from './Recipe';
-
-// const SearchPage = () => {
-// 	const APP_ID = 'dcd2ff3e';
-// 	const APP_KEY = '34c2380ac6cb8b04f580a3e848986ecd';
-
-// 	const [recipes, setRecipes] = useState([]);
-// 	const [search, setSearch] = useState('');
-// 	const [query, setQuery] = useState('');
-// 	// const [diet, setDiet] = useState('');
-
-// 	useEffect(() => {
-// 		getRecipes();
-// 	}, [query]);
-
-// 	const getRecipes = async () => {
-// 		const response = await fetch(
-// 			`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&health=alcohol-free`
-// 		);
-// 		const data = await response.json();
-// 		console.log(data.hits);
-// 		setRecipes(data.hits);
-// 	};
-
-// 	// const updateDiet = (e) => {
-// 	// 	setDiet(e.target.value);
-// 	// };
-
-// 	const updateSearch = (e) => {
-// 		setSearch(e.target.value);
-// 	};
-
-// 	const getSearch = (e) => {
-// 		e.preventDefault();
-// 		setQuery(search);
-// 		// setDiet(diet);
-// 		setSearch('');
-// 		// setDiet('');
-// 	};
-
-// 	return (
-// 		<div className="App">
-// 			<form onSubmit={getSearch} className="search-form">
-// 				<input
-// 					className="search-bar"
-// 					type="text"
-// 					value={search}
-// 					onChange={updateSearch}
-// 				/>
-// 				<select>
-// 					<option value="none"></option>
-// 					<option value="Balanced">Balanced</option>
-// 					<option value="High-Fiber">High-Fiber</option>
-// 					<option value="High-Protein">High-Protein</option>
-// 					<option value="Low-Carb">Low-Carb</option>
-// 					<option value="Low-Fat">Low-Fat</option>
-// 					<option value="Low-Sodium">Low-Sodium</option>
-// 				</select>
-// 				<button className="search-button" type="submit">
-// 					submit
-// 				</button>
-// 			</form>
-// 			<div className="recipes">
-// 				{recipes.map((recipe) => (
-// 					<Recipe
-// 						key={recipe.recipe.label}
-// 						title={recipe.recipe.label}
-// 						calories={recipe.recipe.calories}
-// 						image={recipe.recipe.image}
-// 						ingredients={recipe.recipe.ingredients}
-// 						healthLabels={recipe.recipe.healthLabels}
-// 					/>
-// 				))}
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default SearchPage;
